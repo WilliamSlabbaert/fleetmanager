61893848f3b8ffd89747e0b0a4aaaff2fda92b61
William Slabbaert <56199868+WilliamSlabbaert@users.noreply.github.com>
2021-09-10 15:45:05 +0200

    add vehicleRepo/chaffeurRepo/FuelCardRepo

--- Sourcecode/FleetManager/DataLayer/FleetManagerContext.cs
+++ Sourcecode/FleetManager/DataLayer/FleetManagerContext.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using DataLayer.entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataLayer
 {
     public class FleetManagerContext : DbContext
     {
+        public DbSet<ChaffeurEntity> Chaffeurs { get; set; }
+        public DbSet<FuelCardChaffeurEntity> FuelCardChaffeurs { get; set; }
+        public DbSet<FuelCardEntity> FuelCards { get; set; }
+        public DbSet<InvoiceEntity> Invoices { get; set; }
+        public DbSet<MaintenanceEntity> Maintenances { get; set; }
+        public DbSet<RepairmentEntity> Repairments { get; set; }
+        public DbSet<RequestEntity> Requests { get; set; }
+        public DbSet<VehicleEntity> Vehicles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"ConnectionString");
+        }
     }
 }
---
+++ Sourcecode/FleetManager/DataLayer/repositories/ChaffeurRepo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.repositories
+{
+    public class ChaffeurRepo 
+    {
+        FleetManagerContext context;
+
+        public ChaffeurRepo(FleetManagerContext context)
+        {
+            this.context = context;
+        }
+        public ChaffeurEntity Get(int id)
+        {
+            return this.context.Chaffeurs.FirstOrDefault(s => s.Id == id);
+        }
+        public List<ChaffeurEntity> GetAll()
+        {
+            return this.context.Chaffeurs.ToList();
+        }
+        public void Add(ChaffeurEntity chaffeur)
+        {
+            if (chaffeur != null)
+            {
+                this.context.Chaffeurs.Add(chaffeur);
+            }
+        }
+        public void Delete(int id)
+        {
+            this.context.Remove(this.Get(id));
+        }
+        public void Update(int id, ChaffeurEntity chaffeur)
+        {
+            this.context.Entry(chaffeur).State = EntityState.Modified;
+
+        }
+    }
+}
