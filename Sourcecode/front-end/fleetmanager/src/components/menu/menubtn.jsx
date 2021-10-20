77a02be4706da2bea6e274c33b06adc341bb0aa8
William Slabbaert <56199868+WilliamSlabbaert@users.noreply.github.com>
2021-10-20 15:47:40 +0200

    add router

--- Sourcecode/front-end/fleetmanager/src/components/menu/menubtn.jsx
+++ Sourcecode/front-end/fleetmanager/src/components/menu/menubtn.jsx
@@ -8,9 +8,13 @@ import UserIcon from './style/user_icon.png'
 import OffIcon from './style/logout_icon.png'
 import ReactTooltip from 'react-tooltip';
 import { AppContext } from '../app/appcontext';
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
 
 const MenuBtn = () => {
-    const {menuName, setMenuName} = useContext(AppContext)
+    const { menuName, setMenuName } = useContext(AppContext)
 
     const HandelClick = () => {
         if (menuName[0] === "menuBtn") {
@@ -22,27 +26,37 @@ const MenuBtn = () => {
 
     return (
         <div>
-            <ReactTooltip />
-            <div onClick={HandelClick} className={menuName[0]}>
-                <img src={MenuIcon} alt="Hamburger icon" style={{ "width": "50px" }} />
-            </div>
-            <div className={menuName[1]}>
-                <div className="menuItem" style={{ "marginTop": "5px" }}>
-                    <img data-tip="Car list" src={CarIcon} alt="Car list" style={{ "width": "50px" }} />
+            <Router>
+                <ReactTooltip />
+                <div onClick={HandelClick} className={menuName[0]}>
+                    <img src={MenuIcon} alt="Hamburger icon" style={{ "width": "50px" }} />
                 </div>
-                <div className="menuItem"  style={{ "marginTop": "70px" }}>
-                    <img data-tip="Fuelcard list" src={FuelcardIcon} alt="Fuel card list" style={{ "width": "50px" }} />
+                <div className={menuName[1]}>
+                    <div className="menuItem" style={{ "marginTop": "5px" }}>
+                        <Link to="/carpage" exact style={{ "display": "contents" }}>
+                            <img data-tip="Car list" src={CarIcon} alt="Car list" style={{ "width": "50px" }} />
+                        </Link>
+                    </div>
+                    <div className="menuItem" style={{ "marginTop": "70px" }}>
+                        <Link to="/fuelcardpage" exact style={{ "display": "contents" }}>
+                            <img data-tip="Fuelcard list" src={FuelcardIcon} alt="Fuel card list" style={{ "width": "50px" }} />
+                        </Link>
+                    </div>
+                    <div className="menuItem" style={{ "marginTop": "145px" }}>
+                        <Link to="/requestpage" exact style={{ "display": "contents" }}>
+                            <img data-tip="Request list" src={RequestIcon} alt="Request list" style={{ "width": "50px" }} />
+                        </Link>
+                    </div>
+                    <div className="menuItem" style={{ "marginTop": "215px" }}>
+                        <Link to="/userpage" exact style={{ "display": "contents" }}>
+                            <img data-tip="User list" src={UserIcon} alt="User list" style={{ "width": "50px" }} />
+                        </Link>
+                    </div>
+                    <div className="menuItem logout" style={{ "bottom": "0px", "top": "auto" }}>
+                        <img data-tip="Logout" src={OffIcon} alt="Logout" style={{ "width": "50px" }} />
+                    </div>
                 </div>
-                <div className="menuItem"  style={{ "marginTop": "145px" }}>
-                    <img data-tip="Request list" src={RequestIcon} alt="Request list" style={{ "width": "50px" }} />
-                </div>
-                <div className="menuItem"  style={{ "marginTop": "215px" }}>
-                    <img data-tip="User list" src={UserIcon} alt="User list" style={{ "width": "50px" }} />
-                </div>
-                <div className="menuItem logout"  style={{ "bottom": "0px","top":"auto" }}>
-                    <img data-tip="Logout" src={OffIcon} alt="Logout" style={{ "width": "50px" }} />
-                </div>
-            </div>
+            </Router>
         </div>
     )
 }
