560d1f2708301c1588693ffd81905d66b3afc8cf
William Slabbaert <56199868+WilliamSlabbaert@users.noreply.github.com>
2021-10-25 17:09:45 +0200

    refactor chaffeur to chauffeur BLL and genericresult

--- Sourcecode/FleetManager/BusinessLayer/mediator/queries/GetVehicleKilometerHistoryQuery.cs
+++ Sourcecode/FleetManager/BusinessLayer/mediator/queries/GetVehicleKilometerHistoryQuery.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.mediator.queries
 {
-    public class GetVehicleKilometerHistoryQuery : IRequest<GenericResult<IGeneralModels>>
+    public class GetVehicleKilometerHistoryQuery : IRequest<GenericResult<GeneralModels>>
     {
         public int Id { get; private set; }
 
