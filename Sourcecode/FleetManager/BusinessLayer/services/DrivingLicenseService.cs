407c426702b5fb386720907cd7d6d8a464193778
William Slabbaert <56199868+WilliamSlabbaert@users.noreply.github.com>
2021-10-26 13:55:50 +0200

    refactor drivinglicense service validation

--- Sourcecode/FleetManager/BusinessLayer/services/DrivingLicenseService.cs
+++ Sourcecode/FleetManager/BusinessLayer/services/DrivingLicenseService.cs
@@ -17,6 +17,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLayer.validators;
 
 namespace BusinessLayer.services
 {
@@ -26,32 +27,41 @@ namespace BusinessLayer.services
         private readonly IGenericRepo<ChauffeurEntity> _chrepo;
         private readonly IMapper _mapper;
         private IMediator _mediator;
-        public DrivingLicenseService(IGenericRepo<DrivingLicenseEntity> repo, IMapper mapper, IGenericRepo<ChauffeurEntity> chrepo, IMediator mediator)
+        private IValidator<DrivingLicense> _drivingLicenseValidator;
+        public DrivingLicenseService(IGenericRepo<DrivingLicenseEntity> repo, IMapper mapper, IGenericRepo<ChauffeurEntity> chrepo, IMediator mediator, 
+            DrivingLicenseValidator drivingLicenseV)
         {
             this._repo = repo;
             this._mapper = mapper;
             this._chrepo = chrepo;
             this._mediator = mediator;
+            this._drivingLicenseValidator = drivingLicenseV;
         }
-        public GenericResult<GeneralModels> AddDrivingLicense(DrivingLicenseDTO drivinglicense, int chaffeurid)
+        public GenericResult<GeneralModels> AddDrivingLicense(DrivingLicenseDTO dto, int chaffeurid)
         {
-            var temp = _mapper.Map<DrivingLicense>(drivinglicense);
-            var ch = GetChauffeurEntity(chaffeurid);
-            var dl = _mapper.Map<DrivingLicenseEntity>(temp);
-            var check = CheckExistingDrivingLicense(chaffeurid, temp);
-            var result = new GenericResult<GeneralModels>() { Message = "Drivinglicense already exist's in chaffeurs list." };
-            if (check == false)
+            var drivingLicense = _mapper.Map<DrivingLicense>(dto);
+            var check = _drivingLicenseValidator.Validate(drivingLicense);
+            var response = GenericValidationCheck.CheckModel(check, "Drivinglicense is invalid.");
+            if (check.IsValid)
             {
+                var chaffeurEntity = GetChauffeurEntity(chaffeurid);
+                var dl = _mapper.Map<DrivingLicenseEntity>(drivingLicense);
+                var checkDrivinglicense = CheckExistingDrivingLicense(chaffeurid, drivingLicense);
+                var result = new GenericResult<GeneralModels>() { Message = "Drivinglicense already exist's in chaffeurs list." };
+                if (checkDrivinglicense == false)
+                {
+                    return result;
+                }
+
+                chaffeurEntity.DrivingLicenses.Add(dl);
+                _chrepo.UpdateEntity(chaffeurEntity);
+                _chrepo.Save();
+                result.Message = "Ok";
+                result.SetStatusCode(Overall.ResponseType.OK);
+                result.ReturnValue = _mapper.Map<Chauffeur>(chaffeurEntity);
                 return result;
             }
-
-            ch.DrivingLicenses.Add(dl);
-            _chrepo.UpdateEntity(ch);
-            _chrepo.Save();
-            result.Message = "Ok";
-            result.SetStatusCode(Overall.ResponseType.OK);
-            result.ReturnValue = _mapper.Map<Chauffeur>(ch);
-            return result;
+            return response;
         }
         public GenericResult<GeneralModels> DeleteDrivingLicense(int drivinglicense, int chaffeurid)
         {
